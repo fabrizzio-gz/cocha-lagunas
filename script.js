c2ed33baa7af4670283b621be025485b607fb2d9
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-04-06 12:22:28 -0400

    (feat) Move intro elements with scroll from mid-screen

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 const svg = document.getElementById("cocha");
 
+const titulo = document.getElementById("titulo");
+const continuar = document.getElementById("continuar");
+
+const init = () => {
+  // Move titulo and continuar to absolute positions
+  // with no transform
+  const posTitle = titulo.getBoundingClientRect();
+  const yTitle = posTitle.y;
+  const heightTitle = posTitle.height;
+  const posCont = continuar.getBoundingClientRect();
+  const yCont = posCont.y;
+  const heightCont = posCont.height;
+  titulo.style.transform = "translate(-50%,0)";
+  continuar.style.transform = "translate(-50%,0)";
+
+  titulo.style.top = yTitle + "px";
+  continuar.style.top = yCont + "px";
+
+  return { yTitle, yCont };
+};
+
+const { yTitle, yCont } = init();
+
 document.addEventListener("scroll", function (e) {
-  console.log(window.scrollY);
-  /*
-  lastKnownScrollPosition = window.scrollY;
+  let yPos = window.scrollY;
 
-  if (!ticking) {
-    window.requestAnimationFrame(function() {
-      doSomething(lastKnownScrollPosition);
-      ticking = false;
-    });
-
-    ticking = true;
-  }*/
+  window.requestAnimationFrame(() => {
+    titulo.style.top = yTitle - yPos + "px";
+    continuar.style.top = yCont + yPos + "px";
+  });
 });
