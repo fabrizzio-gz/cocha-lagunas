fc0b9738d355d83a681da85a0f91066bffc59c75
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-04-12 22:17:59 -0400

    (feat) Section 6 animate caption

--- script.js
+++ script.js
@@ -279,39 +279,11 @@ const sec5Anim = anime
   });
 
 const sec6Anim = anime({
-  targets: "#lag-cuellar",
-  begin: () => {
-    const rocha = new Caption("Río Rocha", "cercado-svg-caption-rocha");
-    rocha.div.style.transform = "translate(30%, -80%)";
-    new Caption("La Tamborada", "cercado-svg-caption-tamborada");
-    const cuellar = new Caption(
-      "Laguna Cuellar",
-      "cercado-svg-caption-cuellar"
-    );
-    cuellar.div.style.transform = "translate(-50%, -240%)";
-    const quilla = new Caption(
-      "Puente Quillacollo",
-      "cercado-svg-caption-quillacollo"
-    );
-    quilla.div.style.transform = "translate(-50%, -50%)";
-    new Caption("Recoleta", "cercado-svg-caption-recoleta");
-  },
+  targets: ["#lag-cuellar", "#cercado-svg-caption-cuellar"],
   fill: [lightblue, grey],
   opacity: [1, 0],
   easing: "easeInOutCubic",
   duration: 5000,
-  complete: () => {
-    Caption.purge();
-    const rocha = new Caption("Río Rocha", "cercado-svg-caption-rocha");
-    rocha.div.style.transform = "translate(30%, -80%)";
-    new Caption("La Tamborada", "cercado-svg-caption-tamborada");
-    const quilla = new Caption(
-      "Puente Quillacollo",
-      "cercado-svg-caption-quillacollo"
-    );
-    quilla.div.style.transform = "translate(-50%, -50%)";
-    new Caption("Recoleta", "cercado-svg-caption-recoleta");
-  },
   autoplay: false,
 });
 
