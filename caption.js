cfc053fcab1f2fd5989607341a23de952d8624f7
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-04-23 11:13:46 -0400

    (fix) Set all translates to -50%, -50%
    To position captions correctly.

--- caption.js
+++ caption.js
@@ -107,87 +107,80 @@ class Caption {
       {
         id: "cercado-svg-caption-rocha",
         text: "Río Rocha",
-        translate: "translate(-40%, -170%)",
       },
+      { id: "cercado-svg-caption-recoleta", text: "Recoleta" },
       { id: "cercado-svg-caption-tamborada", text: "La Tamborada" },
       {
         id: "cercado-svg-caption-cuellar",
         text: "Laguna Cuellar",
-        translate: "translate(0, 120%)",
       },
       {
         id: "cercado-svg-caption-quenamari",
         text: "Laguna Quenamari",
-        translate: "translate(-50%, -80%)",
       },
       {
         id: "cercado-svg-caption-alalay",
         text: "Laguna Alalay",
-        translate: "translate(-50%, -80%)",
       },
       {
         id: "cercado-svg-caption-cona-cona",
         text: "Laguna Coña Coña",
-        translate: "translate(-50%, -150%)",
       },
       {
         id: "cercado-svg-caption-quillacollo",
         text: "Puente Quillacollo",
-        translate: "translate(-50%, -70%)",
       },
-      { id: "cercado-svg-caption-recoleta", text: "Recoleta" },
+
       {
         id: "cercado-svg-caption-cuadras",
         text: "Laguna Cuadras",
-        translate: "translate(-50%,-200%)",
       },
       {
         id: "cercado-svg-caption-sarco",
         text: "Laguna Sarco",
-        translate: "translate(-70%, -200%)",
       },
       {
         id: "libertadores",
         text: "Av. Libertadores",
-        translate: "translate(-40%, -100%)  rotate(75deg)",
+        translate: "translate(-50%, -50%) rotate(75deg)",
         isAvenida: true,
       },
       {
         id: "juan-rosa",
         text: "Av. Juan de la Rosa",
-        translate: "translate(-80%, -120%)  rotate(28deg)",
+        translate: "translate(-50%, -50%) rotate(28deg)",
         isAvenida: true,
       },
       {
         id: "america",
         text: "Av. América",
-        translate: "translate(150%, 30%)  rotate(3deg)",
+        translate: "translate(-50%, -50%) rotate(3deg)",
         isAvenida: true,
       },
       {
         id: "melchor-perez",
         text: "Av. Melchor Pérez",
-        translate: "translate(-65%, 750%) rotate(-85deg)",
+        translate: "translate(-50%, -50%) rotate(-85deg)",
 
         isAvenida: true,
       },
       {
         id: "heroinas",
         text: "Av. Heroínas",
-        translate: "translate(20%, -70%) rotate(-10deg)",
+        translate: "translate(-50%, -50%) rotate(-10deg)",
 
         isAvenida: true,
       },
       {
         id: "belzu",
         text: "Av. Belzu",
-        translate: "translate(10%, 350%) rotate(65deg)",
+        translate: "translate(-50%, -50%) rotate(65deg)",
         isAvenida: true,
       },
       {
         id: "campus-umss",
         text: "UMSS",
-        translate: "translate(20%, -40%) rotate(-10deg)",
+        translate: "translate(-50%, -50%) rotate(-10deg)",
         isEdificio: true,
       },
       {
